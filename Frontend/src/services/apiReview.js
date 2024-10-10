e05b61fe9e414107eb5826692334047942e74f34
Ahmed Tarek <123866562+ahmedtarek536@users.noreply.github.com>
2024-10-10 16:31:48 +0300

    Update apiReview.js

--- Frontend/src/services/apiReview.js
+++ Frontend/src/services/apiReview.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://airbnb-rho-teal.vercel.app";
+const apiUrl = "https://airbnb-backend-eta.vercel.app";
 
 async function AddReview(data) {
   const token = localStorage.getItem("token");
